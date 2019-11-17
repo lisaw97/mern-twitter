9ce9c3f9a637a557bf799d86a365c53855b1ef22
lisaw97 <lisawen0614@gmail.com>
2019-11-16 23:03:59 -0800

    Create user authentication

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
     
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.get("/", (req, res) => {
+//     const user = new user({
+//         handle: "lisa",
+//         email: "lisa@gmail.com",
+//         password: "password"
+//     })
+//     user.save()
+//     res.send("Hello World")
+// });
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 
 const port = process.env.PORT || 5000;
 
